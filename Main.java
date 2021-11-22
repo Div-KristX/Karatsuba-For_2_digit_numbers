5e0ce7a5f1c5e78bfdbf6b32cdeacb0e86e07619
Vladyslav <70589107+Div-KristX@users.noreply.github.com>
2021-11-22 16:18:13 +0200

    Uppdate

--- Main.java
+++ Main.java
@@ -2,10 +2,10 @@ package Pegasus;
 
 public class Main {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-Method Multiply = new Method();
-System.out.println("Write two numbers between -99 and 99");
-Multiply.getNumbers();
-    }
+    Method Multiply = new Method();
+    System.out.println("Write two numbers between -99 and 99");
+    Multiply.getNumbers();
+  }
 }
